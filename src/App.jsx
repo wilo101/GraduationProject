3c8c2b0931971598ea7314de474c254ddbdd1a81
Abdelrahman Walaa <200023294@must.edu.eg>
2025-12-16 05:28:33 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import Dashboard from './pages/Dashboard'
 import Settings from './pages/Settings'
